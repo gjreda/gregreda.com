827abbfa3ec68673b1d39d874325dfdc3f9ccd1e
Greg Reda <gjreda+github@gmail.com>
2017-01-07 18:54:40 -0600

    Remove mathjax

--- pelicanconf.py
+++ pelicanconf.py
@@ -34,7 +34,7 @@ TAG_FEED_ATOM = "feeds/tag/%s.atom.xml"
 
 # PLUGINS
 PLUGIN_PATHS = ['pelican-plugins', 'pelican_dynamic']
-PLUGINS = ['assets', 'liquid_tags.notebook', 'pelican_dynamic', 'render_math']
+PLUGINS = ['assets', 'liquid_tags.notebook', 'pelican_dynamic']
 
 CODE_DIR = 'code'
 NOTEBOOK_DIR = 'notebooks'
